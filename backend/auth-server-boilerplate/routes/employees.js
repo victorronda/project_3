4b95ef258b96ae42525425d2f5ca6be7e97ac568
Amalia Glez <glezgutierrez95@gmail.com>
2020-04-09 00:02:24 +0200

    employees and plates routes almost done

--- backend/auth-server-boilerplate/routes/employees.js
+++ backend/auth-server-boilerplate/routes/employees.js
@@ -14,69 +14,65 @@ const {
   validationLoggin
 } = require("../helpers/middlewares")
 
-router.get("/me", isLoggedIn(), async (req, res, next) => { // Traemos el id del empleado y lo buscamos dentro de la empresa
-  const { _id } = req.session.currentUser;
-  const user = await Company.findById({_id}).populate("employees");
-  user.password = "******";
-  res.json(user);
-})
-
+// Login employee (para acceder al client side)
 router.post("/login", isNotLoggedIn(), async (req, res, next) => { // Cuando se loguee tendran que coincidir las keys con algun objeto de los que haya en Company
   const { name, password } = req.body;
   try {
-    const user = await Employee.findOne({ name }).populate("employees");
-
-    if (!user) {
-      next(createError(404));
-    }
-
-    else if (bcrypt.compareSync(password, user.password)) {
-      req.session.currentUser = user;
-      res
-        .status(200)
-        .json(user)
-    }
-    else {
-      next(createError(401));
-    }
+    const user = await Employee.findOne({ name })
+      if (!user) {
+        next(createError(404));
+      }
+      else if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.status(200).json(user);
+      }
+      else {
+        next(createError(401));
+      }
   }
   catch (error) {
     next(error)
   }
-})
-
-router.get("/:companyId", isLoggedIn(), async (req, res, next) => { // Editamos y borramos en base al Id que nos dara mongo o al de companyId?
-  const { companyId } = req.params;
-  const user = await Company.findById({ "_id": companyId }).populate("employees");
-
-  res.json(user)
-})
-
-router.put("/edit", isLoggedIn(), async (req, res, next) => {
-  const { _id } = req.session.currentUser;
-  const { name } = req.body;
-  console.log(name);
-  let updatedUser;
-  if (name) {
-    updatedUser = await Employee.findByIdAndUpdate({ _id }, { $set: { name } }, { new: true })
-}
-
-router.delete("/:_id", async (req, res, next) => { // Aquí si estoy más segura de que el Id que le crea mongo es del que tiraremos para borrarlo
-    const { _id } = req.params
-    const del = await Employee.findByIdAndDelete({ "_id": _id })
-    const employeesUpdated = await Employee.find({ "user": req.session.currentUser._id })
-
-    req.session.currentUser.Employees = employeesUpdated;
-    res.json(req.session.currentUser);
-})
-
-// Con esta ruta comprobamos en postman
-router.get('/private', isLoggedIn(), (req, res, next) => { 
-  res
-    .status(200)
-    .json({ message: 'Test - User is logged in' });
 });
-  res.json(updatedUser)
+
+// Employee list
+router.get('/staff', isLoggedIn(), async (req, res, next) => { // No estoy segura si tendrá que buscarlos con el companyId para que salgan todos los que tiene contratado la empresa
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Edit employee
+router.put("/:_id/edit", isLoggedIn(), async (req, res, next) => {
+  const { _id } = req.session.currentUser;
+  const { name, password } = req.body;
+  //console.log(name);
+  try {
+    const updatedEmployee = await Employee.findByIdAndUpdate(_id, 
+    { $push: { name, password } },
+    { new: true },
+    );
+    res.status(200).json(updatedEmployee);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete employee 
+router.delete("/:_id/delete", async (req, res, next) => { // o staff/:_id/delete???????
+    const { _id } = req.params;
+    if (!_id) {
+      res.status(404).json({message: 'id not found'});
+    }
+    try {
+      await Employee.findByIdAndDelete({ _id });
+      res.status(200).json({ message: 'Employee deleted' });
+    } catch (error) {
+      next(error);
+    }
 })
 
 module.exports = router;
--- backend/auth-server-boilerplate/routes/plates.js
+++ backend/auth-server-boilerplate/routes/plates.js
@@ -1,13 +1,5 @@
 // Amalia
 // Añadir, editar y borrar platos
-/*
-ME PERDI!
-En Plato.js no tenemos vinculación con ningún otro modelo porque ya este se encontraba
-dentro de Menu.js y pensamos que no hacía falta
-Pregunta: A la hora de crearlos desde la página principal del admin (Add Plate), 
-de donde se hace el GET? A partir de que Id? Sea cual sea ese Id, va a tener que estar 
-dentro de Plato.js? O se puede crear de la "nada"?
-*/
 
 const express = require("express");
 const router = express.Router();
@@ -19,45 +11,48 @@ const Menu = require("../models/Menu");
 const { isLoggedIn } = require("../helpers/middlewares");
 router.use(isLoggedIn()); // Si no pusieramos este .use tendriamos que llamar a la función en cada ruta
 
-router.get("/createPlate", async (req, res, next) => {
-    const plates = await Plate.find().populate("company")
-    res.json(plates)
-})
-
-router.get("/:companyId", async (req, res, next) => {
-    const { companyId } = req.params;
-    const plate = await Plate.findById({ "_id": plateId })
-    res.json(plate)
-})
-
-router.post("/", async (req, res, next) => { 
+// Create plate
+router.post("/add/plate", async (req, res, next) => { 
     const { name, typeItem, ingredients, description, image, price, quantity } = req.body;
     const companyId = req.session.currentUser._id;
-    const plate = await Plate.create({name, typeItem, ingredients, description, image, price, quantity});
+    const plate = await Plate.create({ name, typeItem, ingredients, description, image, price, quantity });
 
-    const updatedPlate = await Plate.findByIdAndUpdate({ "_id": companyId }, { $push: { plate: plate._id } }, { new: true });
-
-    req.session.currentUser = updatedPlate;
+    req.session.currentUser = await Plate.findByIdAndUpdate(
+        { "_id": companyId }, 
+        { $push: { plate: plate._id } }, // En este caso, habrá que hacer el push a Menu para que entre en el array que tiene de plates?
+        { new: true }
+        );
 
     res.json(plate);
 })
 
-router.put("/:plateId", async (req, res, next) => {
-    const { plateId } = req.params
-    const { name, typeItem, ingredients, description, image, price, quantity } = req.body
-    const updatedPlate = await Plate.findByIdAndUpdate({ "_id": plateId }, {
-        $set: {name, typeItem, ingredients, description, image, price, quantity}}, { new: true });
+// Edit plate
+router.put("/plate/:_id/edit", async (req, res, next) => {
+    const { _id } = req.params;
+    const { name, typeItem, ingredients, description, image, price, quantity } = req.body;
+    const updatedPlate = await Plate.findByIdAndUpdate(
+        { "_id": _id }, 
+        { $set: {name, typeItem, ingredients, description, image, price, quantity} }, 
+        { new: true });
 
-    res.json(updatedPlate)
+    res.status(200).json(updatedPlate);
 })
 
-router.delete("/:plateId", async (req, res, next) => {
-    const { plateId } = req.params
-    const del = await Plate.findByIdAndDelete({ "_id": plateId })
-    const platesUpdated = await Plate.find({ "user": req.session.currentUser._id })
+// Delete plate
+router.delete("/plate/:_id/delete", async (req, res, next) => {
 
-    req.session.currentUser.plates = platesUpdated;
-    res.json(req.session.currentUser);
-})
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+    
+    try {
+        await Plate.findByIdAndRemove(req.params.id);
+        res.status(200).json({ message: `Plate with ${req.params.id} deleted.` });
+    } catch (err) {
+        next(error)
+    }
+
+});
 
 module.exports = router;
