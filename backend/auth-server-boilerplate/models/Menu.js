49d944afde7daf446a95d32cdb5e4d52c4cc0242
Víctor Ronda <victor.ronda@gmail.com>
2020-04-09 20:32:01 +0200

    auth dishes employees menus routes

--- backend/auth-server-boilerplate/models/Menu.js
+++ backend/auth-server-boilerplate/models/Menu.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const menuSchema = new Schema({
   name: String,
-  dishes: [{type: Schema.Types.ObjectId, ref: "Dish"}],
+  dishes: [ {type: Schema.Types.ObjectId, ref: "Dish"} ] ,
   companyId: [{type: Schema.Types.ObjectId, ref: "Company"}]
 }, {
   timestamps: {
--- backend/auth-server-boilerplate/routes/tables.js
+++ backend/auth-server-boilerplate/routes/tables.js
@@ -1,3 +1,9 @@
-// Añadir, editar y borrar mesas
-// Tener en cuenta borrar platos de la mesa con cada cliente (y el imput de la cantidad de platos)
-// Renderizar tarjetitas amarillas al admin
+// Añadir y editar mesas --> Añadir (Amalia) y editar (Víctor)
+
+
+
+// Tener en cuenta borrar platos de la mesa con cada cliente (y el imput de la cantidad de platos) --> Amalia
+
+
+
+// Renderizar tarjetitas amarillas al admin --> GET que liste todas las mesas con platos --> Víctor
