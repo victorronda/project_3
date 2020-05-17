dd1a387c29c17117f5f0235f3612f1bc16706586
Víctor Ronda <victor.ronda@gmail.com>
2020-05-17 10:55:58 +0200

    home component styling done

--- frontend/auth-client-boilerplate/src/pages/Home.js
+++ frontend/auth-client-boilerplate/src/pages/Home.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import { withAuth } from '../context/AuthProvider'
-import { Link } from "react-router-dom"
-import NavbarEm from '../components/global/NavbarEm'
-
-
-
+import React from 'react';
+import { withAuth } from '../context/AuthProvider';
+import { Link } from 'react-router-dom';
+import NavbarEm from '../components/global/NavbarEm';
 
 const Home = () => {
-
-
-
-
-    const mainStyle = {
-        margin: "26.5vh auto"
-    }
+	const mainStyle = {
+		margin: '26vh auto'
+    };
     
-    return (
-        <div>
-        <NavbarEm/>
-        <div className="d-block text-center align-content-center " style={mainStyle}>
-            <div>
-            <h1 style={{fontSize: "120px"}}>MGBITE</h1>
-            </div>
-            <div className="mt-5">
-            <Link style={{textDecoration: "none", color: "black", boxShadow: "0.1px 0.2px 0.2px 0.1px gray"}} className="px-5 py-2 my-5 signupButton" to="/signup">Signup</Link><br/>
-            </div>
-            
-            <div className="mt-5">
-                <p className="d-inline mr-3" style={{fontWeight: "bolder"}}><b>Already registered?</b></p>
-                <Link style={{textDecoration: "none", color: "#576064"}} className="linksB" to="/employees/login">Login</Link>
+    const titleStyle = {
+        fontSize: '9vw'
+    }
 
-            </div>
-        </div>
-        </div>
-    )
-}
 
-export default withAuth(Home)
+	return (
+		<div>
+			<NavbarEm />
+			<div className="d-block text-center align-content-center " style={mainStyle}>
+				<div>
+					<h1 style={titleStyle}>MGBITE</h1>
+				</div>
+				<div className="mt-5">
+					<Link style={{textDecoration: 'none' }} className="px-5 py-2 my-5 signupButton" to="/signup">
+					<b>Sign up</b>	
+					</Link>
+					<br />
+				</div>
+
+				<div className="mt-5">
+					<p className="d-inline mr-3" style={{ fontSize: '0.8vw' }}>
+						<b>Already registered?</b>
+					</p>
+					<Link style={{textDecoration: 'none'}} className="linksB" to="/employees/login">
+						Login
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default withAuth(Home);
